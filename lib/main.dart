2463f1f25d19d42bfe94c5d6c754c1204230673b
XRandolphX <randolphpeluche@gmail.com>
2022-11-16 16:21:31 -0500

    Cambio sutil

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practica_s12/etapa2.dart';
+import 'package:practica_s12/vistaPrincipal.dart';
 
 //Importaciones FIREBASE
 import 'package:cloud_firestore/cloud_firestore.dart';
